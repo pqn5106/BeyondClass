ed5d04c660043e19a2187e99d459534c8423bc35
Param Nigam <pqn5106@psu.edu>
2023-07-13 20:04:48 -0400

    rebased

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,24 @@
+import Image from "next/image"
+import { styled } from '@mui/system';
+import Button from '@mui/material/Button';
+import * as React from 'react';
 export default function Home() {
   return (
     <main>
+      <nav>
+        <Image src="/logo.png" width={100} height={50} />
+          <Button variant="contained" size="small">Login</Button>
+          <Button variant="outlined" size="small">Sign Up</Button>
+      </nav>
+
       <section id="highlight-view">
-        
+        <p>hi</p>
       </section>
       <h1>HELLO</h1>
       
-      <section id="trending-view">
+      <section id="trending-view" className = 'bg-black'>
+        <p>
+hi</p>
       </section>
 
       <section id="upcoming-view">
