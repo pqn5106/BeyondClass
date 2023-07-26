f527d1387d303f78ea11aac8c1e5ee4f0ab0adc6
RyanJaiH <97144551+RyanJaiH@users.noreply.github.com>
2023-07-26 12:24:46 +0100

    Added coming soon pages, navbar won't render correctly

---
+++ pages/competition.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import Navbar from "@/components/Navbar";
+
+const Competition = () => {
+
+  return (
+    <main>
+      <Navbar/>
+
+      <section id="trending-view" className="bg-[#edeef2]">
+          <h1 className="text-3xl text-[#24245b] font-semibold px-20 py-5 text-center">Competition Opportunities Coming Soon!</h1>
+      </section>
+    </main>
+  )
+}
+
+export default Competition
---
+++ pages/network.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import Navbar from "@/components/Navbar";
+
+const Network = () => {
+
+  return (
+    <main>
+      <Navbar/>
+
+      <section id="trending-view" className="bg-[#edeef2]">
+          <h1 className="text-3xl text-[#24245b] font-semibold px-20 py-5 text-center">Network Opportunities Coming Soon!</h1>
+      </section>
+    </main>
+  )
+}
+
+export default Network
